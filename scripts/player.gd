9b3802cc03862ef99f8d11f5b71db39049f677a6
supersigma <pkneerad@gmail.com>
2026-05-09 22:48:25 +0530

    Player weapon anim

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,14 +4,32 @@ const MOVE_SPEED : int = 150
 const ACCELERATION : int = 600
 const DECELLERATION : int = 1200
 
+
+@onready var weapon_sprite: Sprite2D = $Marker2D/weapon
+
+var weapon_angle_ovveride : bool = false
+var attack_cooldown_timer : float = 0
+
 func _physics_process(delta: float) -> void:
 	var up_down_direction : float = Input.get_axis("up", "down")
+	var left_right_direction : float = Input.get_axis("left", "right")
+	
+	attack_cooldown_timer -= delta
+	attack_cooldown_timer = clamp(attack_cooldown_timer, 0, INF)
+	up_down_direction = Vector2(left_right_direction, up_down_direction).normalized().y
+	left_right_direction = Vector2(left_right_direction, up_down_direction).normalized().x
+	
+	if Input.is_action_just_pressed("attack") and attack_cooldown_timer == 0:
+		match Global.inventory_items[Global.focused_slot]:
+			Global.items.PISTOL:
+				$AnimationPlayer.play("pistol")
+				attack_cooldown_timer = $AnimationPlayer.get_animation("pistol").length
+	
 	if up_down_direction:
 		velocity.y = move_toward(velocity.y, MOVE_SPEED * up_down_direction, ACCELERATION * delta)
 	else:
 		velocity.y = move_toward(velocity.y, 0, DECELLERATION * delta)
 	
-	var left_right_direction : float = Input.get_axis("left", "right")
 	if left_right_direction:
 		velocity.x = move_toward(velocity.x, MOVE_SPEED * left_right_direction, ACCELERATION * delta)
 		$AnimatedSprite2D.flip_h = !left_right_direction > 0
@@ -23,14 +41,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		$AnimatedSprite2D.play("idle")
 	
-	$Marker2D.look_at(get_global_mouse_position())
-	if $Marker2D.global_rotation_degrees > 90 or $Marker2D.global_rotation_degrees < -90:
-		$Marker2D/weapon.rotation_degrees = 180
-		$Marker2D/weapon.flip_h = true
-	elif !$Marker2D.global_rotation_degrees > 90 or !$Marker2D.global_rotation_degrees < -90:
-		$Marker2D/weapon.rotation_degrees = 0
-		$Marker2D/weapon.flip_h = false
-	$Marker2D/weapon.texture = Global.items_list_textures[Global.inventory_items[Global.focused_slot]]
+	
+	if !weapon_angle_ovveride:
+		$Marker2D.look_at(get_global_mouse_position())
+		if $Marker2D.global_rotation_degrees > 90 or $Marker2D.global_rotation_degrees < -90:
+			weapon_sprite.rotation_degrees = 180
+			weapon_sprite.flip_h = true
+		elif !$Marker2D.global_rotation_degrees > 90 or !$Marker2D.global_rotation_degrees < -90:
+			weapon_sprite.rotation_degrees = 0
+			weapon_sprite.flip_h = false
+		weapon_sprite.texture = Global.items_list_textures[Global.inventory_items[Global.focused_slot]]
 	
 	if Input.is_action_just_pressed("inventory1"):
 		Global.focused_slot = 1
@@ -44,3 +64,14 @@ func _physics_process(delta: float) -> void:
 		Global.focused_slot = 5
 	
 	move_and_slide()
+
+
+func perpendicularize_sword() -> void:
+	weapon_angle_ovveride = true
+	if weapon_sprite.flip_h:
+		weapon_sprite.rotation_degrees = -270
+	else:
+		weapon_sprite.rotation_degrees = 90
+
+func unperpendicularize_sword() -> void:
+	weapon_angle_ovveride = false
