c8a76d80644b6ba35e1062315c7e8aa4473201a3
supersigma <pkneerad@gmail.com>
2026-05-10 13:22:11 +0530

    Added bullet shooting and dagger stabbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,21 +9,38 @@ const DECELLERATION : int = 1200
 
 var weapon_angle_ovveride : bool = false
 var attack_cooldown_timer : float = 0
+var weapon_target_rotation_degrees : float = 0
+
+var pistol_bullet : PackedScene = preload("res://scenes/pistolbullet.tscn")
 
 func _physics_process(delta: float) -> void:
 	var up_down_direction : float = Input.get_axis("up", "down")
 	var left_right_direction : float = Input.get_axis("left", "right")
+	weapon_sprite.global_rotation_degrees = lerp(weapon_sprite.global_rotation_degrees, weapon_target_rotation_degrees, 0.1)
+	
 	
 	attack_cooldown_timer -= delta
 	attack_cooldown_timer = clamp(attack_cooldown_timer, 0, INF)
 	up_down_direction = Vector2(left_right_direction, up_down_direction).normalized().y
 	left_right_direction = Vector2(left_right_direction, up_down_direction).normalized().x
 	
-	if Input.is_action_just_pressed("attack") and attack_cooldown_timer == 0:
+	if Input.is_action_pressed("attack") and attack_cooldown_timer == 0:
+		$AnimationPlayer.stop()
 		match Global.inventory_items[Global.focused_slot]:
+			
 			Global.items.PISTOL:
 				$AnimationPlayer.play("pistol")
-				attack_cooldown_timer = $AnimationPlayer.get_animation("pistol").length
+				var bullet : Area2D = pistol_bullet.instantiate()
+				bullet.global_rotation = $Marker2D.global_rotation
+				bullet.global_position = $Marker2D/weapon.global_position
+				add_sibling(bullet)
+				
+				attack_cooldown_timer = $AnimationPlayer.get_animation("pistol").length / $AnimationPlayer.speed_scale
+			Global.items.DAGGER:
+				$AnimationPlayer.play("dagger")
+				attack_cooldown_timer = $AnimationPlayer.get_animation("pistol").length / $AnimationPlayer.speed_scale
+	
+	
 	
 	if up_down_direction:
 		velocity.y = move_toward(velocity.y, MOVE_SPEED * up_down_direction, ACCELERATION * delta)
@@ -32,7 +49,7 @@ func _physics_process(delta: float) -> void:
 	
 	if left_right_direction:
 		velocity.x = move_toward(velocity.x, MOVE_SPEED * left_right_direction, ACCELERATION * delta)
-		$AnimatedSprite2D.flip_h = !left_right_direction > 0
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECELLERATION * delta)
 	
@@ -47,9 +64,11 @@ func _physics_process(delta: float) -> void:
 		if $Marker2D.global_rotation_degrees > 90 or $Marker2D.global_rotation_degrees < -90:
 			weapon_sprite.rotation_degrees = 180
 			weapon_sprite.flip_h = true
+			$AnimatedSprite2D.flip_h = true
 		elif !$Marker2D.global_rotation_degrees > 90 or !$Marker2D.global_rotation_degrees < -90:
 			weapon_sprite.rotation_degrees = 0
 			weapon_sprite.flip_h = false
+			$AnimatedSprite2D.flip_h = false
 		weapon_sprite.texture = Global.items_list_textures[Global.inventory_items[Global.focused_slot]]
 	
 	if Input.is_action_just_pressed("inventory1"):
@@ -64,14 +83,3 @@ func _physics_process(delta: float) -> void:
 		Global.focused_slot = 5
 	
 	move_and_slide()
-
-
-func perpendicularize_sword() -> void:
-	weapon_angle_ovveride = true
-	if weapon_sprite.flip_h:
-		weapon_sprite.rotation_degrees = -270
-	else:
-		weapon_sprite.rotation_degrees = 90
-
-func unperpendicularize_sword() -> void:
-	weapon_angle_ovveride = false
